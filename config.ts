81c3e7438aa78ac9b34ea933f97c12938b80b710
Jackson <jrobe3221@gmail.com>
2021-05-06 16:01:28 -0500

    rowed

---
+++ config.ts
@@ -0,0 +1,13 @@
+export const name = "PrinterBot";
+export const version = '0.0.1';
+
+export const reactionEmoteSuccess = "✅";
+export const reactionEmoteFail = "❌";
+
+export const discordToken = "nil";
+export const channelId = "nil";
+
+export const token = "nil";
+export const url = "nil";
+
+export const interval = 2000;
